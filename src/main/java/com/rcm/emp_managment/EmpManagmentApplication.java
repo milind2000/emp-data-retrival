3d62ecd7ba027af68fc8a1ea1812316c72f34b34
Milind Phalke <45395090+milind2000@users.noreply.github.com>
2025-06-30 23:23:48 +0530

    Initial Project setup (#1)

---
+++ src/main/java/com/rcm/emp_managment/EmpManagmentApplication.java
@@ -0,0 +1,16 @@
+package com.rcm.emp_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManagmentApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello there.. This is the first commit. Edited");
+		System.out.println("Master Merge was successfull");
+		SpringApplication.run(EmpManagmentApplication.class, args);
+	}
+
+}
