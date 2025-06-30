18270e21a30cc1e020f430cbd2545a5c5c473a03
Milind Phalke <milindphalke30@gmail.com>
2025-06-30 23:11:30 +0530

    Initial commit

---
+++ src/main/java/com/rcm/emp_managment/EmpManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.rcm.emp_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcm/emp_managment/EmpManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcm.emp_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
