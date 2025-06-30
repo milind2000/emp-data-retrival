f6c399b763614c9ae6ae4029226a26c15f9ac6d7
Milind Phalke <milindphalke30@gmail.com>
2025-06-30 23:22:57 +0530

    In sync with master

--- src/main/java/com/rcm/emp_managment/EmpManagmentApplication.java
+++ src/main/java/com/rcm/emp_managment/EmpManagmentApplication.java
@@ -9,6 +9,7 @@ public class EmpManagmentApplication {
 	public static void main(String[] args) {
 
 		System.out.println("Hello there.. This is the first commit. Edited");
+		System.out.println("Master Merge was successfull");
 		SpringApplication.run(EmpManagmentApplication.class, args);
 	}
 
